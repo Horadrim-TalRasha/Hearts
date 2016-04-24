b30f8e9d22f5028387094a895b1a75360667675f
spitbreak <524948250@qq.com>
2016-04-24 19:11:22 +0800

    Revert "declare function 'InitCourt'"

--- game/court.h
+++ game/court.h
@@ -1,16 +1,8 @@
 //--------------- Revision List -----------------------
 //   2016-04-01  lichao  create version
-//   2016-04-24  lichao  add array of coms
-//   2016-04-24  lichao  declare function InitCourt
 //-----------------------------------------------------
 
 
 #include "com.h"
 
 #define MAX_COM 4
-
-
-static com szComs[MAX_COM];
-
-void
-InitCourt();
