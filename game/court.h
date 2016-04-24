e8ca495c00e07f5c350face62e06264fe5ad74ab
li--chao <tianguodexiaoge@hotmail.com>
2016-04-24 18:57:08 +0800

    declare InitCourt

--- game/court.h
+++ game/court.h
@@ -1,6 +1,7 @@
 //--------------- Revision List -----------------------
 //   2016-04-01  lichao  create version
 //   2016-04-24  lichao  add array of coms
+//   2016-04-24  lichao  declare function InitCourt
 //-----------------------------------------------------
 
 
@@ -10,3 +11,6 @@
 
 
 static com szComs[MAX_COM];
+
+void
+InitCourt();
