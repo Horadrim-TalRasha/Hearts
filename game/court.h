60d645c50024383607f5459413caec2f0c998d8e
spitbreak <524948250@qq.com>
2016-04-24 18:06:17 +0800

    declare coms

--- game/court.h
+++ game/court.h
@@ -1,8 +1,12 @@
 //--------------- Revision List -----------------------
 //   2016-04-01  lichao  create version
+//   2016-04-24  lichao  add array of coms
 //-----------------------------------------------------
 
 
 #include "com.h"
 
 #define MAX_COM 4
+
+
+static com szComs[MAX_COM];
