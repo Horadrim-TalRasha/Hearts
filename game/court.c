dce805cf994559dafb902cf4016d23b017bf4f3b
li--chao <tianguodexiaoge@hotmail.com>
2016-04-30 23:15:48 +0800

    add empty function InitCourt

--- game/court.c
+++ game/court.c
@@ -1,6 +1,14 @@
 //--------------- Revision List -----------------------
 //   2016-04-01  lichao  create version
+//   2016-04-30  lichao  add a empty function of InitCourt
 //-----------------------------------------------------
 
 
 #include "court.h"
+
+void InitCourt()
+{
+
+
+
+}
